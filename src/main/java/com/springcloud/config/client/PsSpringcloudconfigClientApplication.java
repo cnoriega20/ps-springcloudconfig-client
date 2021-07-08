4d42794cae7f886e4cad0ad2954498a1accb7404
Cesar Noriega <c79.noriega@gmail.com>
2021-07-08 00:32:28 -0500

    Initial commit: Created config server client

---
+++ src/main/java/com/springcloud/config/client/PsSpringcloudconfigClientApplication.java
@@ -0,0 +1,13 @@
+package com.springcloud.config.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsSpringcloudconfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PsSpringcloudconfigClientApplication.class, args);
+    }
+
+}
